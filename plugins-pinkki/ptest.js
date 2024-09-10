fbea9785f0e1dc969345bf7543bea5aa747f0083
vitech <i@vitechliu.com>
2024-09-10 19:21:13 +0800

    调整

--- plugins-pinkki/ptest.js
+++ plugins-pinkki/ptest.js
@@ -2,48 +2,13 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-ygopro.ctos_follow_before('CHAT', true, async (buffer, info, client, server, datas) => {
+ygopro.ctos_follow_before('JOIN_GAME', false, async (buffer, info, client, server, datas) => {
+    /** @type {Room} */
     const room = ROOM_all[client.rid] ?? null;
-    if (!room) {
-        return false;
-    }
+    if (!room) return false;
+    console.log('[JOIN_GAME]')
+    console.log('RoomName: ' + room.name)
+    
+
     ygopro.stoc_send_chat_to_room(room, "cc1", ygopro.constants.COLORS.RED);
-});
-ygopro.ctos_follow('CHAT', true, async (buffer, info, client, server, datas) => {
-    const room = ROOM_all[client.rid] ?? null;
-    if (!room) {
-        return false;
-    }
-    ygopro.stoc_send_chat_to_room(room, "cc2", ygopro.constants.COLORS.RED);
-});
-ygopro.ctos_follow_after('CHAT', true, async (buffer, info, client, server, datas) => {
-    const room = ROOM_all[client.rid] ?? null;
-    if (!room) {
-        return false;
-    }
-    ygopro.stoc_send_chat_to_room(room, "cc3", ygopro.constants.COLORS.RED);
-});
-
-
-//异步测试
-ygopro.ctos_follow_before('CHAT', false, async (buffer, info, client, server, datas) => {
-    const room = ROOM_all[client.rid] ?? null;
-    if (!room) {
-        return false;
-    }
-    ygopro.stoc_send_chat_to_room(room, "cc4", ygopro.constants.COLORS.RED);
-});
-ygopro.ctos_follow('CHAT', false, async (buffer, info, client, server, datas) => {
-    const room = ROOM_all[client.rid] ?? null;
-    if (!room) {
-        return false;
-    }
-    ygopro.stoc_send_chat_to_room(room, "cc5", ygopro.constants.COLORS.RED);
-});
-ygopro.ctos_follow_after('CHAT', false, async (buffer, info, client, server, datas) => {
-    const room = ROOM_all[client.rid] ?? null;
-    if (!room) {
-        return false;
-    }
-    ygopro.stoc_send_chat_to_room(room, "cc6", ygopro.constants.COLORS.RED);
 });
