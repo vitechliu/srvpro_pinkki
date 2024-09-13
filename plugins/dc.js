d8f312c58cf8acd6435c62b857585216fe7feba3
Vitech <i@vitechliu.com>
2024-09-14 02:54:13 +0800

    Update dc.js

--- plugins/dc.js
+++ plugins/dc.js
@@ -36,7 +36,9 @@ async function generateDeck(client, server, room, failMessage) {
     // utils.optimizeClientDeck(updateInfo, client)
     console.log("UpdateInfo:");
     console.log(updateInfo)
-    ygopro.ctos_send(server, "UPDATE_DECK", updateInfo);
+    const promise = ygopro.ctos_send(server, "UPDATE_DECK", updateInfo);
+    console.log(promise)
+    
 }
 
 ygopro.ctos_follow_before("UPDATE_DECK", false, async (buffer, info, client, server, datas) => {
