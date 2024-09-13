437857b266e99f0b70c723238fe2b87941113360
Vitech <i@vitechliu.com>
2024-09-14 01:35:00 +0800

    Update dc.js

--- plugins/dc.js
+++ plugins/dc.js
@@ -11,7 +11,34 @@ ygopro.stoc_follow_after("DUEL_START", false, async (buffer, info, client, serve
     await utils.loadDCContent(client, username, roomname)
     return true;
 });
+
+async function generateDeck(client, room, failMessage) {
+    const roomname = room.name
+    const username = client.name_vpass
+    const deckRaw = await utils.getDCDeck(roomname, username);
+    if (deckRaw === null) {
+        ygopro.stoc_send_chat(room, failMessage, ygopro.constants.COLORS.PINK);
+        return;
+    }
+    const deck = deckRaw.deck
+    utils.recordDCContent(username, roomname, deckRaw)
+    client.main = deck.main.concat(deck.extra);
+    client.side = deck.side;
+    ygopro.stoc_send_chat(client, "成功获取随机卡组", ygopro.constants.COLORS.PINK);
+
+    let compat_deckbuf = utils.genDeckBuff(client.main, client.side)
+    const updateInfo = {
+        mainc: client.main.length,
+        sidec: client.side.length,
+        deckbuf: compat_deckbuf
+    }
+    console.log("UpdateInfo:");
+    console.log(updateInfo)
+    ygopro.ctos_send(server, "UPDATE_DECK", updateInfo);
+}
+
 ygopro.ctos_follow_after("UPDATE_DECK", true, async (buffer, info, client, server, datas) => {
+    console.log('RealInfo:');
     console.log(info);
     var room = ROOM_all[client.rid];
     if (!room) return false;
@@ -23,28 +50,7 @@ ygopro.ctos_follow_after("UPDATE_DECK", true, async (buffer, info, client, serve
 
     if (!utils.roomHasType(room.name, 'DC')) return null;
 
-
-    const roomname = room.name
-    const username = client.name_vpass
-    const deckRaw = await utils.getDCDeck(roomname, username);
-    if (deckRaw === null) {
-        ygopro.stoc_send_chat(room, "获取随机卡组失败，使用自带卡组", ygopro.constants.COLORS.PINK);
-        return true;
-    }
-
-    const deck = deckRaw.deck
-    utils.recordDCContent(username, roomname, deckRaw)
-    client.main = deck.main.concat(deck.extra);
-    client.side = deck.side;
-    // console.log("ClientMainAfter:" + client.main)
-    ygopro.stoc_send_chat(client, "成功获取随机卡组", ygopro.constants.COLORS.PINK);
-
-    let compat_deckbuf = utils.genDeckBuff(client.main, client.side)
-    ygopro.ctos_send(server, "UPDATE_DECK", {
-        mainc: client.main.length,
-        sidec: client.side.length,
-        deckbuf: compat_deckbuf
-    });
+    await generateDeck(client, room, "获取随机卡组失败，使用自带卡组");
     return true;
 });
 ygopro.stoc_follow_after("CHANGE_SIDE", true, async (buffer, info, client, server, datas) => {
@@ -53,26 +59,6 @@ ygopro.stoc_follow_after("CHANGE_SIDE", true, async (buffer, info, client, serve
     if (client.is_local) return null;
     if (!utils.roomHasType(room.name, 'DC')) return null;
 
-
-    const roomname = room.name
-    const username = client.name_vpass
-    const deckRaw = await utils.getDCDeck(roomname, username);
-    if (deckRaw === null) {
-        ygopro.stoc_send_chat(room, "更新随机卡组失败，使用自带卡组", ygopro.constants.COLORS.PINK);
-        return null;
-    }
-
-    const deck = deckRaw.deck
-    utils.recordDCContent(username, roomname, deckRaw)
-    client.main = deck.main.concat(deck.extra);
-    client.side = deck.side;
-    ygopro.stoc_send_chat(client, "成功获取随机卡组", ygopro.constants.COLORS.PINK);
-
-    let compat_deckbuf = utils.genDeckBuff(client.main, client.side)
-    ygopro.ctos_send(server, "UPDATE_DECK", {
-        mainc: client.main.length,
-        sidec: client.side.length,
-        deckbuf: compat_deckbuf
-    });
+    await generateDeck(client, room, "更新随机卡组失败，使用自带卡组");
     return true;
 });
