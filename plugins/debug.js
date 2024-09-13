77c20e5e9b67ecf91f1ae11906c82953c7e00392
Vitech <i@vitechliu.com>
2024-09-14 03:07:51 +0800

    Update debug.js

--- plugins/debug.js
+++ plugins/debug.js
@@ -2,13 +2,13 @@
 const utils = require('../plugins-pinkki/util.js')
 
 const CTOS_EVENTS = ['UPDATE_DECK', 'PLAYER_INFO', 'JOIN_GAME'];
-const STOC_EVENTS = ['CHANGE_SIDE', 'DUEL_START', 'ERROR_MSG', 'DECK_COUNT'];
+const STOC_EVENTS = ['CHANGE_SIDE', 'DUEL_START', 'ERROR_MSG', 'GAME_MSG', 'DECK_COUNT'];
 const DEBUG = true;
 
 if (DEBUG) {
 
     for (let e of CTOS_EVENTS) {
-        ygopro.ctos_follow(e, false, (buffer, info, client, server, datas) => {
+        ygopro.ctos_follow_before(e, false, (buffer, info, client, server, datas) => {
             let infos = ['[CTOS]'];
             infos.push('[' + e + ']')
             if (client.rid) {
@@ -24,7 +24,7 @@ if (DEBUG) {
         });
     }
     for (let e2 of STOC_EVENTS) {
-        ygopro.stoc_follow(e2, false, (buffer, info, client, server, datas) => {
+        ygopro.stoc_follow_before(e2, false, (buffer, info, client, server, datas) => {
             let infos = ['[STOC]'];
             infos.push('[' + e2 + ']')
             if (client && client.rid) {
