06cab660e417279d6da3a2db2e3445c9adbd53ac
Vitech <i@vitechliu.com>
2024-09-15 19:01:38 +0800

    未绑定的账号无法游戏

--- plugins/dc.js
+++ plugins/dc.js
@@ -64,6 +64,15 @@ ygopro.ctos_follow_after("UPDATE_DECK", true, async (buffer, info, client, serve
     const room = getDCRoomFromPlayerClient(client);
     if (!room) return null;
     if (room.duel_stage !== ygopro.constants.DUEL_STAGE.BEGIN) return null;
+
+    const uid = utils.uidGet(client.name_vpass)
+    if (uid === null) {
+        const kickedName = client.name;
+        //踢出
+        ygopro.stoc_send_chat_to_room(room, "[" + kickedName + "]被移出了房间(没有绑定账号，并且找不到同名群账号)", ygopro.constants.COLORS.PINK);
+        global.CLIENT_kick(client);
+        return '_cancel';
+    }
     return true; //跳过DC房间的自主卡组提交阶段
 });
 ygopro.stoc_follow_after("CHANGE_SIDE", true, async (buffer, info, client, server, datas) => {
